458bea61a7c2e7e9e7e6c9f6f3ed0e202efa2ccd
mpearce25 <pearcematthew25@gmail.com>
2016-10-06 18:26:26 -0400

    updated comments

--- sqrtFull.py
+++ sqrtFull.py
@@ -1,6 +1,6 @@
 guess = number = float(input("Enter Number")) #sets the initial guees and the number that you are finding the sqrt of to the input
 
-while(round(guess**2,4) != round(number,4)): #limits the number of gueeses tot he max guesses and stops the loop if the approximation is equal to within 4 decimal places
+while(round(guess**2,4) != round(number,4)): #stops the loop if the approximation is equal to within 4 decimal places
     guess = (guess + (number / guess)) / 2 #sets guess to the average of the distance betweent he current guess (an oversetimate) and the number divided by the current guess (an undersetimate)
 
 print("Estimate:",round(guess,4)) #prints the estimate
