5c22914afca8ade9abff25369ea09e09a001dfaa
mpearce25 <pearcematthew25@gmail.com>
2016-10-07 08:21:37 -0400

    rmeoved blank line

--- sqrtFull.py
+++ sqrtFull.py
@@ -7,12 +7,9 @@ while(not validInput):
     except:
         print("Error")
 
-
-
 while(round(guess**2,4) != round(number,4)): #stops the loop if the approximation is equal to within 4 decimal places
     underEstimate = number / guess
     guess = (guess + underEstimate) / 2 #sets guess to the average of the distance betweent he current guess (an oversetimate) and the number divided by the current guess (an undersetimate)
 
 print("Estimate:",round(guess,4)) #prints the estimate
 print("Actual:\t ", number**.5) #prints the actual value
-
