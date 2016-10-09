32b0f10bcb08038939db33272b9f17ec140ac3e0
mpearce25 <pearcematthew25@gmail.com>
2016-10-08 23:59:44 -0400

    got short version to 5 lines

--- sqrtShort.py
+++ sqrtShort.py
@@ -1,9 +1,5 @@
-number = ""
-while(not number.isdigit()):
-    guess = number = input("Enter Number")
+inputPrompt, number = "Please Enter A Number ", "Enter Number"
+while(not number.isdigit()):  number, inputPrompt = input(inputPrompt), "Invalid Input - Please only enter a positive number"
 number = guess = float(number)
-while(round(guess**2,4) != round(number,4)): #stops the loop if the approximation is equal to within 4 decimal places
-    underEstimate = number / guess
-    guess = (guess + underEstimate) / 2 #sets guess to the average of the distance betweent he current guess (an oversetimate) and the number divided by the current guess (an undersetimate)
-
-print("Estimate:",round(guess,4), "Actual:\t ", number**.5) #prints the estimate
+while(round(guess**2,4) != round(number,4)): guess = (guess + (number / guess)) / 2
+print("Estimate:",round(guess,4), "\nActual:\t ", number**.5)
